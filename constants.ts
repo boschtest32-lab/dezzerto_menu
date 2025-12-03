3da1c3bd6db92a80f98527707d9ea9144fc51ebd
parate.lokesh3050 <parate.lokesh30@gmail.com>
2025-12-03 22:50:57 +0530

    updated images

--- constants.ts
+++ constants.ts
@@ -18,39 +18,39 @@ export const WHATSAPP_NUMBER = "918600135430"; // Country code + Number
 
 export const MENU_ITEMS: MenuItem[] = [
   // Thick Shakes
-  { id: '1', name: 'Belgian Dark Chocolate', description: 'Rich and creamy dark chocolate blend', price: 220, category: Category.THICK_SHAKES, image: 'public/images/thick-shake.jpg' },
-  { id: '2', name: 'Strawberry Cheesecake', description: 'Real strawberries with cheesecake bits', price: 240, category: Category.THICK_SHAKES, image: 'public/images/thick-shake.jpg' },
+  { id: '1', name: 'Belgian Dark Chocolate', description: 'Rich and creamy dark chocolate blend', price: 220, category: Category.THICK_SHAKES, image: 'public/images/thickshake.jpg' },
+  { id: '2', name: 'Strawberry Cheesecake', description: 'Real strawberries with cheesecake bits', price: 240, category: Category.THICK_SHAKES, image: 'public/images/thickshake.jpg' },
   
   // Waffles
   { id: '3', name: 'Nutella Overload', description: 'Crispy waffle topped with Nutella', price: 180, category: Category.WAFFLES, image: 'public/images/waffel.jpg' },
   { id: '4', name: 'Red Velvet', description: 'Red velvet base with white chocolate', price: 190, category: Category.WAFFLES, image: 'public/images/waffel.jpg' },
 
   // Milk Shakes
-  { id: '5', name: 'Classic Vanilla', description: 'Smooth vanilla bean shake', price: 150, category: Category.MILK_SHAKES, image: 'public/images/milk-shake.jpg' },
-  { id: '6', name: 'Oreo Crunch', description: 'Crunchy Oreos blended to perfection', price: 170, category: Category.MILK_SHAKES, image: 'images/milk-shake.jpg' },
+  { id: '5', name: 'Classic Vanilla', description: 'Smooth vanilla bean shake', price: 150, category: Category.MILK_SHAKES, image: 'public/images/milkshake.jpg' },
+  { id: '6', name: 'Oreo Crunch', description: 'Crunchy Oreos blended to perfection', price: 170, category: Category.MILK_SHAKES, image: 'images/milkshake.jpg' },
 
   // Ice Creams
-  { id: '7', name: 'French Vanilla Scoop', description: 'Classic creamy vanilla', price: 90, category: Category.ICE_CREAMS, image: 'public/images/ice-cream.jpg' },
-  { id: '8', name: 'Butterscotch', description: 'Crunchy praline butterscotch', price: 100, category: Category.ICE_CREAMS, image: 'images/ice-cream.jpg' },
+  { id: '7', name: 'French Vanilla Scoop', description: 'Classic creamy vanilla', price: 90, category: Category.ICE_CREAMS, image: 'public/images/Ice-Cream.jpg' },
+  { id: '8', name: 'Butterscotch', description: 'Crunchy praline butterscotch', price: 100, category: Category.ICE_CREAMS, image: 'images/Ice-Cream.jpg' },
 
   // Brownies
-  { id: '9', name: 'Walnut Fudge Brownie', description: 'Warm gooey fudge with walnuts', price: 120, category: Category.BROWNIES, image: 'public/images/brownie.jpg' },
-  { id: '10', name: 'Sizzling Brownie', description: 'Served with vanilla ice cream', price: 180, category: Category.BROWNIES, image: 'images/brownie.jpg' },
+  { id: '9', name: 'Walnut Fudge Brownie', description: 'Warm gooey fudge with walnuts', price: 120, category: Category.BROWNIES, image: 'public/images/hot-sizzling-brownie.jpg' },
+  { id: '10', name: 'Sizzling Brownie', description: 'Served with vanilla ice cream', price: 180, category: Category.BROWNIES, image: 'images/hot-sizzling-brownie.jpg' },
 
   // Hot Brew
   { id: '11', name: 'Cappuccino', description: 'Espresso with steamed milk foam', price: 140, category: Category.HOT_BREW, image: 'public/images/hot-brew.jpg' },
   { id: '12', name: 'Americano', description: 'Double shot espresso with hot water', price: 110, category: Category.HOT_BREW, image: 'images/hot-brew.jpg' },
 
   // Cold Brew
-  { id: '13', name: 'Classic Cold Brew', description: 'Steeped for 18 hours', price: 160, category: Category.COLD_BREW, image: 'public/images/cold-brew.jpg' },
-  { id: '14', name: 'Vietnam Cold Coffee', description: 'Cold brew with condensed milk', price: 180, category: Category.COLD_BREW, image: 'public/images/cold-brew.jpg' },
+  { id: '13', name: 'Classic Cold Brew', description: 'Steeped for 18 hours', price: 160, category: Category.COLD_BREW, image: 'public/images/Cold-Coffee.jpg' },
+  { id: '14', name: 'Vietnam Cold Coffee', description: 'Cold brew with condensed milk', price: 180, category: Category.COLD_BREW, image: 'public/images/Cold-Coffee.jpg' },
 
   // Jamun Shots
-  { id: '15', name: 'Classic Jamun Shot', description: 'Pure jamun pulp with spice mix', price: 80, category: Category.JAMMUN_SHOTS, image: 'public/images/jamun-shot.jpg' },
+  { id: '15', name: 'Classic Jamun Shot', description: 'Pure jamun pulp with spice mix', price: 80, category: Category.JAMMUN_SHOTS, image: 'public/images/jamunshots.jpg' },
 
   // Hot Chocolate
-  { id: '16', name: 'Classic Hot Cocoa', description: 'Rich cocoa with marshmallows', price: 190, category: Category.HOT_CHOCOLATE, image: 'public/images/hot-chocolate.jpg' },
-  { id: '17', name: 'Spiced Hot Chocolate', description: 'Cocoa with a hint of cinnamon', price: 210, category: Category.HOT_CHOCOLATE, image: 'public/images/hot-chocolate.jpg' },
+  { id: '16', name: 'Classic Hot Cocoa', description: 'Rich cocoa with marshmallows', price: 190, category: Category.HOT_CHOCOLATE, image: 'public/images/hot-brew.jpg' },
+  { id: '17', name: 'Spiced Hot Chocolate', description: 'Cocoa with a hint of cinnamon', price: 210, category: Category.HOT_CHOCOLATE, image: 'public/images/hot-brew.jpg' },
 ];
 
 export const CATEGORIES = Object.values(Category);
